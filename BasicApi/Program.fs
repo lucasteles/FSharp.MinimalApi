76820e6341bb5e455573e0d26c7ec873a6e89db0
Lucas Teles <lucas.teles@outlook.com>
2023-11-14 14:22:51 -0300

    minor: new endpoints builder api (based on AsParametersAttribute)

--- BasicApi/Program.fs
+++ BasicApi/Program.fs
@@ -8,8 +8,6 @@ open BasicApi.Models
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Mvc
 open Microsoft.EntityFrameworkCore
-open Microsoft.AspNetCore.Http
-open Microsoft.AspNetCore.Http.HttpResults
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
@@ -17,6 +15,8 @@ open FSharp.MinimalApi
 open FSharp.MinimalApi.Swagger
 open FSharp.MinimalApi.Builder
 open Microsoft.Extensions.Options
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Http.HttpResults
 open type TypedResults
 
 let otherRoute =
@@ -42,12 +42,9 @@ let routes =
     endpoints {
         get "/hello" (fun () -> "world")
 
-        get "/ping/{x}" (fun (x: int) -> $"pong {x}")
+        get "/ping/{x}" (fun (req: {| x: int |}) -> $"pong {req.x}")
 
-        get "/inc/{v:int}" (fun (v: int) (n: Nullable<int>) -> v + (n.GetValueOrDefault 1))
-
-        // not working =/
-        get "/arg/{x}" (fun ([<FromRoute(Name = "x")>] v: int) -> $"echo {v}")
+        get "/inc/{v:int}" (fun (req: {| v: int; n: Nullable<int> |}) -> req.v + (req.n.GetValueOrDefault 1))
 
         // access route builder
         get "/secret" (fun () -> "I'm secret") (fun b -> b.ExcludeFromDescription())
@@ -55,28 +52,25 @@ let routes =
         // manual set builder config
         set (fun b -> b.MapGet("/health", (fun () -> "healthy")))
 
-        // using custom Parameters (AsParameterAttribute)
-        get
-            "/params/{foo}"
-            (AsParameters.Of(fun (param: CustomParams) ->
-                param.logger.LogInformation "Hello Params"
-                $"route={param.foo}; query={param.bar}"))
+        get "/params/{foo}" (fun (param: CustomParams) ->
+            param.logger.LogInformation "Hello Params"
+            $"route={param.foo}; query={param.bar}")
 
         // using options from DI
-        get "/settings" (fun (options: IOptions<MyCustomSettings>) -> options.Value)
+        get "/settings" (fun (req: {| options: IOptions<MyCustomSettings> |}) -> req.options.Value)
 
-        get "/double/{v}" produces<Ok<int>> (fun (v: int) -> Ok(v * 2))
+        get "/double/{v}" produces<Ok<int>> (fun (req: {| v: int |}) -> Ok(req.v * 2))
 
         endpoints {
             path "very" "long" "path" "dec"
             get "/" (fun (n: int) -> n - 1)
         }
 
-        get "/even/{v}" produces<Ok<string>, BadRequest> (fun (v: int) (logger: ILogger<_>) ->
-            (if v % 2 = 0 then
+        get "/even/{v}" produces<Ok<string>, BadRequest> (fun (req: {| v: int; logger: ILogger<_> |}) ->
+            (if req.v % 2 = 0 then
                  !! Ok("even number!")
              else
-                 logger.LogInformation $"Odd number: {v}"
+                 req.logger.LogInformation $"Odd number: {req.v}"
                  !! BadRequest()))
 
         otherRoute
@@ -93,15 +87,15 @@ let routes =
                         return! next ctx
                 })
 
-            get "/" produces<Ok<User[]>> (fun (db: MyDbContext) ->
+            get "/" produces<Ok<User[]>> (fun (req: {| db: MyDbContext |}) ->
                 task {
-                    let! users = db.Users.ToArrayAsync()
+                    let! users = req.db.Users.ToArrayAsync()
                     return Ok(users)
                 })
 
-            get "/{userId}" produces<Ok<User>, NotFound> (fun (userId: Guid) (db: MyDbContext) ->
+            get "/{userId}" produces<Ok<User>, NotFound> (fun (req: {| userId: Guid; db: MyDbContext |}) ->
                 task {
-                    let! res = db.Users.Where(fun x -> x.Id = UserId userId).TryFirstAsync()
+                    let! res = req.db.Users.Where(fun x -> x.Id = UserId req.userId).TryFirstAsync()
 
                     match res with
                     | Some user -> return !! Ok(user)
@@ -114,30 +108,30 @@ let routes =
                 post
                     "/"
                     produces<Created<User>, Conflict, ValidationProblem>
-                    (fun (userInfo: NewUser) (db: MyDbContext) ->
+                    (fun (req: {| userInfo: NewUser; db: MyDbContext |}) ->
                         task {
-                            match NewUser.parseUser userInfo with
+                            match NewUser.parseUser req.userInfo with
                             | Error err -> return !! ValidationProblem(err)
                             | Ok newUser ->
-                                let! exists = db.Users.TryFirstAsync(fun x -> x.Email = newUser.Email)
+                                let! exists = req.db.Users.TryFirstAsync(fun x -> x.Email = newUser.Email)
 
                                 match exists with
                                 | Some _ -> return !! Conflict()
                                 | None ->
-                                    db.Users.add newUser
-                                    do! db.saveChangesAsync ()
+                                    req.db.Users.add newUser
+                                    do! req.db.saveChangesAsync ()
                                     return !! Created($"/user/{newUser.Id.Value}", newUser)
                         })
 
-                delete "/{userId}" produces<NoContent, NotFound> (fun (userId: Guid) (db: MyDbContext) ->
+                delete "/{userId}" produces<NoContent, NotFound> (fun (req: {| userId: Guid; db: MyDbContext |}) ->
                     task {
-                        let! exists = db.Users.TryFirstAsync(fun x -> x.Id = UserId userId)
+                        let! exists = req.db.Users.TryFirstAsync(fun x -> x.Id = UserId req.userId)
 
                         match exists with
                         | None -> return !! NotFound()
                         | Some user ->
-                            db.Users.remove user
-                            do! db.saveChangesAsync ()
+                            req.db.Users.remove user
+                            do! req.db.saveChangesAsync ()
                             return !! NoContent()
                     })
 
--- FSharp.MinimalApi.Interop/AsParameter.cs
+++ FSharp.MinimalApi.Interop/AsParameter.cs
@@ -6,10 +6,23 @@ using Microsoft.AspNetCore.Http;
 public static class AsParameters
 {
     public static Delegate Of<TParam, TResult>(FSharpFunc<TParam, TResult> requestDelegate) =>
-        ([AsParameters] TParam parameters) =>
-            requestDelegate.Invoke(parameters);
+        typeof(TParam) == typeof(Unit)
+            ? typeof(TResult) == typeof(Unit)
+                ? void () => requestDelegate.Invoke(Operators.Unchecked.DefaultOf<TParam>())
+                : () => requestDelegate.Invoke(Operators.Unchecked.DefaultOf<TParam>())
+            : typeof(TResult) == typeof(Unit)
+                ? void ([AsParameters] TParam parameters) => requestDelegate.Invoke(parameters)
+                : ([AsParameters] TParam parameters) => requestDelegate.Invoke(parameters);
 
-    public static Delegate Of<TParam, TResult>(FSharpFunc<TParam, Task<TResult>> requestDelegate) =>
-        ([AsParameters] TParam parameters) =>
-            requestDelegate.Invoke(parameters);
+    public static Delegate OfTask<TParam, TResult>(
+        FSharpFunc<TParam, Task<TResult>> requestDelegate) =>
+        typeof(TParam) == typeof(Unit)
+            ? typeof(TResult) == typeof(Unit)
+                ? Task ([AsParameters] TParam parameters) =>
+                requestDelegate.Invoke(Operators.Unchecked.DefaultOf<TParam>())
+                : ([AsParameters] TParam parameters) =>
+                requestDelegate.Invoke(Operators.Unchecked.DefaultOf<TParam>())
+            : typeof(TResult) == typeof(Unit)
+                ? Task ([AsParameters] TParam parameters) => requestDelegate.Invoke(parameters)
+                : ([AsParameters] TParam parameters) => requestDelegate.Invoke(parameters);
 }
